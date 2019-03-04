d12c2f0b72b0b7ade4997d79c41f041e62547170
naaltunian <naaltunian@icloud.com>
2019-03-04 14:18:14 -0500

    create input component and add search and handleChange functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Input from './Components/Input/Input';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    movies: [],
+    searchQuery: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchQuery: e.target.value })
+  }
+
+  search = async (e) => {
+    e.preventDefault();
+    // let { data } = await axios.get(``);
+    console.log(this.state.searchQuery);
+    this.setState({ searchQuery: "" })
+  }
+
   render() {
     return (
       <div>
         <Navbar />
+        <Input searchQuery={this.state.searchQuery} handleChange={this.handleChange} search={this.search} />
       </div>
     );
   }
