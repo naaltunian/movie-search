4e51d301f68ba06ae70619613af0687af0074050
naaltunian <naaltunian@icloud.com>
2019-03-04 18:48:19 -0500

    Render movie data on screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Input from './Components/Input/Input';
+import Movie from './Components/Movie/Movie';
 import axios from 'axios';
 import apiKey from './config';
 
@@ -31,7 +32,11 @@ class App extends Component {
       <div>
         <Navbar />
         <Input searchQuery={this.state.searchQuery} handleChange={this.handleChange} search={this.search} />
-        
+        <div>
+          {this.state.movies.length > 0 && this.state.movies.map(movie => {
+            return <Movie movie={movie} key={movie.id} />
+          })}
+        </div>
       </div>
     );
   }
