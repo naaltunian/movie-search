aa3f8afae30e57885ca713b37769a2503bd20daf
naaltunian <naaltunian@icloud.com>
2019-03-04 19:19:19 -0500

    Style Movie Cards

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import "./Movie.css";
 
 const Movie = (props) => {
     const movie = props.movie;
     return(
-        <div>
+        <div className="card">
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie poster" height="300px" width="200px" />
+            <span className=".card-info">
             <h2>{movie.title} ({movie.release_date})</h2>
             <h5>{movie.overview}</h5>
+            </span>
         </div>
     )
 }
